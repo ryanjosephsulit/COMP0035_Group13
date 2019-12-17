e279b92527c7d45c2156da776f1f02aa72904b8a
Ryan Joseph Sulit Velasquez <zcecrjs@ucl.ac.uk>
2019-12-17 14:52:24 +0000

    Upload of User Account class by Ryan, as well as test_UserAccount

---
+++ UserAccount.py
@@ -0,0 +1,67 @@
+#COMP0035: Systems Engineering
+#Individual Coursework
+
+class UserAccount:
+
+    def __init__(self, user_account):
+        self.UserAccount = user_account
+
+#Defining class attributes
+
+    @property
+    def useraccount(self):
+        pass
+
+    def name(self):
+        pass
+
+    def UserID(self):
+        pass
+
+    def Fluent_Languages(self):
+        pass
+
+    def language_ability(lang_level):
+        pass
+
+    def location(self):
+        pass
+
+    def email_address(self):
+        pass
+
+    def phone_number(self):
+        pass
+
+#Defining class methods
+
+    def login(self):
+        pass
+
+    def logout(self):
+        pass
+
+    def edit_account_attributes(self):
+        pass
+
+    def edit_UI_language(self):
+        pass
+
+    def edit_location(location):
+        print ("Location changed to " + location)
+        return location
+
+
+    def edit_language_ability(self):
+        pass
+
+    def view_post(self):
+        pass
+
+    def view_reviews(self):
+        pass
+
+if __name__ == "__main__":
+    main()
+
+
